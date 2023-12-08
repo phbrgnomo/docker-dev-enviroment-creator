690338a11c353f22ac56c60daaf498b55e6932c2
phbr <34484531+phbrgnomo@users.noreply.github.com>
2023-12-08 11:53:32 -0300

    Prettier outputs

--- buildbase.sh
+++ buildbase.sh
@@ -3,11 +3,8 @@
 # Build the base development environment image with Dockerfile configuration
 docker build -t base_env .
 
-
-# Prompt to enter the bash shell
-:<<'COMMENT'
-read -p "Do you want to enter the container's bash shell? (y/n): " answer
+# Prompt to enter the container's bash shell
+read -p $'\e[94mDo you want to enter the container\'s bash shell? (y/n):\e[0m ' answer
 if [ "$answer" == "y" ]; then
     docker run -it --rm base_env /bin/sh
 fi
-COMMENT
--- newproject.sh
+++ newproject.sh
@@ -8,36 +8,38 @@ fi
 
 # Check if the project folder already exists
 if [ -d "$1" ]; then
-    echo "Warning: Project folder '$1' already exists. Choose a different project name."
+    echo -e "\e[91mError:\e[0m Project folder \e[93m'$1'\e[0m already exists. Choose a different project name."
     exit 1
 fi
 
 # Create a new project folder
 mkdir "$1"
 
-    # Check if a template name is provided
+# Check if a template name is provided
 if [ -n "$2" ]; then
     template=$2
 
     # Check if template files exist, otherwise create empty templates
     if [ -f "templates/${template}.Dockerfile" ]; then
         cp "templates/${template}.Dockerfile" "$1/Dockerfile"
+        echo -e "\e[92mSuccess:\e[0m Using template \e[93m'${template}.Dockerfile'\e[0m."
     else
-        echo "Warning: Template ${template}.Dockerfile not found. Using base Dockerfile."
+        echo -e "\e[93mWarning:\e[0m Template ${template}.Dockerfile not found. Using base Dockerfile."
         cp "Dockerfile" "$1/"
-        echo "Base Dockerfile copied to the project folder."
+        echo -e "\e[92mSuccess:\e[0m Base Dockerfile copied to the project folder."
     fi
 
     if [ -f "templates/${template}.docker-compose.yml" ]; then
         cp "templates/${template}.docker-compose.yml" "$1/docker-compose.yml"
+        echo -e "\e[92mSuccess:\e[0m Using template \e[93m'${template}.docker-compose.yml'\e[0m."
     else
-        echo "Warning: Template ${template}.docker-compose.yml not found. Creating empty docker-compose.yml."
+        echo -e "\e[93mWarning:\e[0m Template ${template}.docker-compose.yml not found. Creating empty docker-compose.yml."
         touch "$1/docker-compose.yml"
-        echo "Empty docker-compose.yml created in the project folder."
+        echo -e "\e[92mSuccess:\e[0m Empty docker-compose.yml created in the project folder."
     fi
 fi
 
 # Optionally, open VSCode in the new project folder
 # code .
 
-echo "Project '$1' created successfully. Happy coding!"
+echo -e "\e[92mSuccess:\e[0m Project \e[93m'$1'\e[0m created successfully. Happy coding!"
